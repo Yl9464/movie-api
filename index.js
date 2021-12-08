440e260725a21aa0326dc7a19a67e8959d5d551c
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-12-07 21:19:25 -0500

    returns request by title

---
+++ index.js
@@ -0,0 +1,22 @@
+const { request } = require('express')
+const express = require('express')
+const movies = require('./movies')
+const app = express()
+
+// 1) A request for all data [X]
+// 2) A request for all movies by a director
+// 3) A request for a movie by title. Note that you should support partial matches as shown below.
+
+app.get('/', (request, response) => {
+  return response.send({ movies })
+})
+
+app.get('/:title', (request, response) => {
+  const { title } = request.params
+  const findTitle = movies.filter(name => name.title === title)
+
+  return response.send(findTitle)
+})
+
+
+app.listen(1345)
