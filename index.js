3bbe3212e09622b1ab3b0e7244a2c644809aad46
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-12-09 11:13:57 -0500

    tying match method

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const { request } = require('express')
 const express = require('express')
 const movies = require('./movies')
 const app = express()
 
 // 1) A request for all data [X]
 // 2) A request for all movies by a director
-// 3) A request for a movie by title. Note that you should support partial matches as shown below.[X]
+// 3) A request for a movie by title. Note that you should support partial matches as shown below.[1/2]
 
 app.get('/', (request, response) => {
   return response.send({ movies })
 })
 
+// edit so it returns partial matches
 app.get('/:title', (request, response) => {
   const { title } = request.params
-  const findTitle = movies.filter(name => name.title === title)
+  const findTitle = movies.filter(name => name.title === title || name.title.match(/''/g))
 
   return response.send(findTitle)
 })
 
 
-app.listen(1345)
+app.listen((1345), () => {
+  console.log('Listening on http://localhost:1345...')
+})
 
