3ea9f698f4bb567748e52cc2b23f89973a8fbae1
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2021-12-23 18:44:41 -0500

    final commit

--- controller/movies.js
+++ controller/movies.js
@@ -1,10 +1,24 @@
-
 const movies = require('../movies')
 
 const getAllMovies = (request, response) => {
-  return response.send(movies)
+  response.send(movies)
+}
+
+
+const movieByDirectorOrTitle = (request, response) => {
+  const { directors } = request.params
+
+  const foundFilm = movies.filter((name) => {
+    return name.title.toLowerCase().includes(directors.toLowerCase()) ||
+      name.directors.find((director) => director.toLowerCase().includes(directors.toLowerCase()))
+  })
+
+  if (!foundFilm.length) return response.sendStatus(404)
+
+  return response.send(foundFilm)
 }
 
 module.exports = {
-  getAllMovies
+  getAllMovies,
+  movieByDirectorOrTitle
 }
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express')
-const { getAllMovies } = require('./controller/movies')
+const { getAllMovies, movieByDirectorOrTitle } = require('./controller/movies')
 const app = express()
-
-// 1) A request for all data [X]
-// 2) A request for all movies by a director
-// 3) A request for a movie by title. Note that you should support partial matches as shown below.
 
 app.get('/', getAllMovies)
 
+app.get('/:directors', movieByDirectorOrTitle)
 
-
-app.listen((1345), () => {
-  console.log('Listening on http://localhost:1345...')
+app.listen(1345, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 1345...')
 })
